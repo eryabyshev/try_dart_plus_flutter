427d7bdda0b0a9481bbd8b8e1a438e31245bcfc1
Evgeny Ryabyshew <ru.evgeny.dev@gmail.com>
2019-08-06 23:59:56 +0300

    sout text ob center

---
+++ helloflutter/firs_app_flutter/first_flutter_app/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    
+    new Center(
+      child: new Text(
+        "Hello, it my first flutter app",
+        textDirection: TextDirection.ltr,
+      )
+    )
+  );
+}
