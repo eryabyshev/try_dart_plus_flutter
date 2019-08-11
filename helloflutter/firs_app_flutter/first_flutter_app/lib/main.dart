6258fb8ca2475f66da947ac139ecd15f2d92c58a
Evgeny Ryabyshew <ru.evgeny.dev@gmail.com>
2019-08-11 14:00:33 +0300

    Material design + dir

--- helloflutter/firs_app_flutter/first_flutter_app/lib/main.dart
+++ helloflutter/firs_app_flutter/first_flutter_app/lib/main.dart
@@ -1,25 +1,7 @@
+import 'package:first_flutter_app/ui/home.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-
-    new Material(
-      color: Colors.lightGreenAccent,
-
-      child: new Center(
-
-        child: new Text (
-          "Hello from mt firs app again",
-          textAlign: TextAlign.center,
-          textDirection: TextDirection.ltr,
-          style: new TextStyle(
-            fontWeight: FontWeight.w500,
-            fontSize: 22
-
-          ),
-        ),
-      )
-    )
-
-  );
-}
+void main() => runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+));
