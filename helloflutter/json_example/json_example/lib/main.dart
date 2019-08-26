cf768c9532837f0f43e155bda85c2394477285e5
Evgeny Ryabyshew <ru.evgeny.dev@gmail.com>
2019-08-26 22:42:33 +0300

    ListView.builder

--- helloflutter/json_example/json_example/lib/main.dart
+++ helloflutter/json_example/json_example/lib/main.dart
@@ -2,28 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
 main() async {
-
   var _data = await getJson();
 
-  runApp(
-      MaterialApp(
-        title: "JSON example",
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("JSON example"),
-            backgroundColor: Colors.orangeAccent,
-          ),
-          body: Text(_data[0]["body"]),
+  runApp(MaterialApp(
+      title: "JSON example",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("JSON example"),
+          backgroundColor: Colors.orangeAccent,
         ),
-      )
-  );
+        body: ListView.builder(
+            itemCount: _data.length,
+            itemBuilder: (BuildContext context, int position) {
+              return Column(
+                children: <Widget>[
+                  Divider(height: 3.4),
+                  ListTile(
+                    title: Text("Title: ${_data[position]["title"]}"),
+                    subtitle: Text("Body: ${_data[position]["body"]}"),
+                    leading: CircleAvatar(
+                      child: Icon(Icons.print, color: Colors.orangeAccent),
+                    ),
+                  )
+                ],
+              );
+            }),
+      )));
 }
 
 Future<List> getJson() async {
-  final response =
-  await http.get('https://jsonplaceholder.typicode.com/posts');
+  final response = await http.get('https://jsonplaceholder.typicode.com/posts');
   if (response.statusCode == 200) {
     // If server returns an OK response, parse the JSON.
     return json.decode(response.body);
